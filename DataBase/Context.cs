520ff0ed38c153a241c2939afd81bafd65465946
iPonzu <joao_ignaciocastro1603@hotmail.com>
2023-07-19 16:41:40 -0300

    repo: start

---
+++ DataBase/Context.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
+using System;
+using Models;
+
+namespace Data{
+    public class Context : DbContext{
+        public string connection = "Server=localhost; User Id=root;Database=warehousesystem";
+        public DbSet<Warehouse> warehouses {get; set;}
+        public DbSet<Balance> balances {get; set;}
+        public DbSet<Product> products {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseMySql(connection, ServerVersion.AutoDetect(connection));
+    }
+}
