f45a59726b8d2afadae32b845bc908a10e1fa9cb
iPonzu <joao_ignaciocastro1603@hotmail.com>
2023-08-02 23:47:42 -0300

    adding migrations to database

--- Migrations/20230803021137_warehouse.Designer.cs
+++ Migrations/20230803021137_warehouse.Designer.cs
@@ -2,6 +2,7 @@
 using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace warehouse.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230803021137_warehouse")]
+    partial class warehouse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
