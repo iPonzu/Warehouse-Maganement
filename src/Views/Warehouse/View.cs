e0f09f305f58b29e0dbe081cdbcdf08211aed974
iPonzu <joao_ignaciocastro1603@hotmail.com>
2023-08-02 23:47:31 -0300

    adding visual warehouse configurations

--- src/Views/Warehouse/View.cs
+++ src/Views/Warehouse/View.cs
@@ -79,8 +79,49 @@ namespace Views{
         }
 
         public WarehouseView(){
-            
+            this.Text = "Warehouse";
+            this.Size = new Size(800, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
 
+            warehouseList = new ListView();
+            warehouseList.Size = new Size(780, 400);
+            warehouseList.Location = new Point(10, 10);
+            warehouseList.View = View.Details;
+            warehouseList.FullRowSelect = true;
+            warehouseList.MultiSelect = false;
+            warehouseList.Columns.Add("Id", -2, HorizontalAlignment.Center);
+            warehouseList.Columns.Add("Name", -2, HorizontalAlignment.Center);
+            warehouseList.Columns.Add("Address", -2, HorizontalAlignment.Center);
+            RefreshList();
+            this.Controls.Add(warehouseList);
+
+            Button btCreate = new Button();
+            btCreate.Text = "Create";
+            btCreate.Size = new Size(100, 30);
+            btCreate.Location = new Point(10, 420);
+            btCreate.Click += new EventHandler(btCreate_Click);
+            this.Controls.Add(btCreate);
+
+            Button btUpdate = new Button();
+            btUpdate.Text = "Update";
+            btUpdate.Size = new Size(100, 30);
+            btUpdate.Location = new Point(120, 420);
+            btUpdate.Click += new EventHandler(btUpdate_Click);
+            this.Controls.Add(btUpdate);
+
+            Button btDelete = new Button();
+            btDelete.Text = "Delete";
+            btDelete.Size = new Size(100, 30);
+            btDelete.Location = new Point(230, 420);
+            btDelete.Click += new EventHandler(btDelete_Click);
+            this.Controls.Add(btDelete);
+
+            Button btClose = new Button();
+            btClose.Text = "Close";
+            btClose.Size = new Size(100, 30);
+            btClose.Location = new Point(340, 420);
+            btClose.Click += new EventHandler(btClose_Click);
+            this.Controls.Add(btClose);
         }
     }
 }
